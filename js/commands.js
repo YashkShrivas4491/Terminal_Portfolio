880e499b2f59827100d7b4aea8f291429d943ae4
Yash Kumar Shrivas <yashkshrivas1234@gmail.com>
2023-04-25 18:55:00 +0530

    Initial Commit 🥳³

---
+++ js/commands.js
@@ -0,0 +1,87 @@
+var youtube = "https://www.youtube.com/channel/UC1gCSSMfOQX57ZkM-9kix_A";
+var twitter = "https://twitter.com/YashKumarS4491/";
+var password = "Yks4491";
+var linkedin = "https://www.linkedin.com/in/yash-kumar-shrivas-98a759126/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/YashkShrivas4491";
+var email = "mailto:kumaryash4491@gmail.com";
+var leetCode = "https://leetcode.com/YashShrivas_4491/";
+var gFG = "https://auth.geeksforgeeks.org/user/yashkshrivas1234/practice";
+var codeChef = "https://www.codechef.com/users/yash_4491";
+
+whois = ["<br>", "Hey, I'm Yash!👋",
+"I always love to contribute in open-source software and learn in public while sharing my knowledge to others developer 🚀",  
+"I do a lot of things including  web development and at times cloud(MsAzure, Google Cloud) related stuff.", "<br>"];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, “What I cannot create, I do not understand.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' +
+    youtube +
+    '" target="_blank">youtube/channel/UC1gCSSMfOQX57ZkM-9kix_A' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/YashKumarS4491/' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/yash-kumar-shrivas-98a759126' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/YashkShrivas4491' +
+    "</a>",
+  'leetCode        <a href="' +
+    leetCode +
+    '" target="_blank">leetcode/YashShrivas_4491/' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>",
+];
+
+
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Yash?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  "#  ╦ ╦┌─┐┌─┐┬ ┬  ╦╔═┬ ┬┌┬┐┌─┐┬─┐  ╔═╗┬ ┬┬─┐┬┬  ┬┌─┐┌─┐",
+  "#  ╚╦╝├─┤└─┐├─┤  ╠╩╗│ ││││├─┤├┬┘  ╚═╗├─┤├┬┘│└┐┌┘├─┤└─┐",
+  "#   ╩ ┴ ┴└─┘┴ ┴  ╩ ╩└─┘┴ ┴┴ ┴┴└─  ╚═╝┴ ┴┴└─┴ └┘ ┴ ┴└─┘",
+
+  '<span class="color2">Hey! Everyone 👋.</span>',
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
